19bce6dd3dcfd9b7556ce72c72c30aa3a6b005cb
TomSchellekens <schellekens.tom@gmail.com>
2021-04-15 14:50:57 +0200

    Hoi

--- FrontEnd/Form1.Designer.cs
+++ FrontEnd/Form1.Designer.cs
@@ -29,8 +29,6 @@
 		private void InitializeComponent()
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.listBox1 = new System.Windows.Forms.ListBox();
-			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// panel1
@@ -40,30 +38,11 @@
 			this.panel1.Size = new System.Drawing.Size(200, 100);
 			this.panel1.TabIndex = 0;
 			// 
-			// listBox1
-			// 
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(478, 93);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(120, 95);
-			this.listBox1.TabIndex = 1;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(302, 104);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.listBox1);
 			this.Controls.Add(this.panel1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -75,8 +54,6 @@
 		#endregion
 
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.ListBox listBox1;
-		private System.Windows.Forms.Button button1;
 	}
 }
 
